52e42c0eb48ffbdc555ad4b7543f92043d8531e9
DuongNguyen <nhatduong30001@gmail.com>
2022-05-30 08:20:29 +0700

    Init Assignment 4

---
+++ Assignment 4/initial/src/main/d96/parser/D96.g4
@@ -0,0 +1,304 @@
+grammar D96;
+// 1952638
+@lexer::header {
+from lexererr import *
+}
+
+options {
+	language = Python3;
+}
+// Default setting
+program: class_declaration+ EOF;
+
+
+
+
+
+// Expression 
+
+exp: relational_operations string_operators relational_operations | relational_operations ;
+
+relational_operations: logical_operations relational_operators logical_operations | logical_operations;
+
+logical_operations: logical_operations logical_and_or adding_operations | adding_operations;
+
+adding_operations: adding_operations adding_operators multiplying_operations | multiplying_operations;
+
+multiplying_operations: multiplying_operations multiplying_operators logical_negate_operation | logical_negate_operation;
+
+logical_negate_operation: NEGATE logical_negate_operation | sign_operations;
+
+sign_operations: MINUS sign_operations | index_operations;
+
+index_operations: index_operations index_operators | intance_access_operarion;
+
+intance_access_operarion: intance_access_operarion DOT static_access_operation
+                       | intance_access_operarion DOT static_access_operation LB expList? RB
+                       | static_access_operation;
+
+static_access_operation: object_create_operation STATIC_ACCESS  DOLLARID LB expList? RB
+                       | object_create_operation STATIC_ACCESS  DOLLARID
+                       | object_create_operation;
+
+
+object_create_operation: NEW ID LB expList? RB| parenthesis_operations;
+
+parenthesis_operations: LB exp RB
+                      |INTLIT | STRINGLIT | BOOLEANLIT | FLOATLIT | ID | array_lit  | SELF | NULL; 
+
+// ArrayLIT dau
+
+
+string_operators: STRINGCOMPARE | STRINGCONCAT;
+
+relational_operators: EQUAL | DIFFERENT | LESSER | LESSTHANEQUAL | GREATER | GREATERTHANEQUAL;
+
+logical_and_or: AND | OR;
+
+adding_operators: PLUS | MINUS;
+
+multiplying_operators: MULTIPLY | DIVIDE | MODULO;
+
+element_expression : exp index_operators; //
+
+index_operators : '[' exp ']' index_operators
+                | '[' exp ']';//
+
+// Array Literature
+array_lit: ARRAYTYPE LB expList? RB;
+
+
+
+// If statement
+
+if_statements: IF LB exp RB block_statements else_ifList? else_statements?;//
+
+else_ifList:  else_if the_rest_else_if;//
+
+the_rest_else_if: else_if the_rest_else_if |;//
+
+else_if: ELSEIF LB exp RB block_statements; //
+
+else_statements: ELSE block_statements;//
+
+// Assignment statement;
+
+assignment_statements: lhs ASSIGN exp SEMI;//
+
+lhs: lhs DOT assignment_static_access
+   | assignment_static_access;
+
+assignment_static_access: (ID | index_operations| SELF) STATIC_ACCESS DOLLARID
+                        | ID | index_operations | SELF;//
+// For/In
+
+foreach_statements: FOREACH LB ID IN  exp '..' exp (BY exp)? RB  block_statements;//
+
+
+// break statements
+
+break_statements: BREAK SEMI;//
+
+// Continue statements
+
+continue_statements: CONTINUE SEMI;//
+
+// return statements
+
+return_statements: RETURN exp? SEMI;//
+
+// Method invocation
+
+method_invocations: exp DOT ID SEMI
+                  | ID STATIC_ACCESS DOLLARID SEMI
+                  | exp DOT ID LB expList? RB SEMI
+                  | ID STATIC_ACCESS DOLLARID LB expList? RB SEMI; //
+
+
+block_statements: LP  list_statement RP; //
+
+list_statement: single_statement the_rest_statements |;//
+
+the_rest_statements: single_statement the_rest_statements |;//
+
+// Variable and Constant Declaration Statement
+variable_constant_declaration: (VAR | VAL) variableList COLON primitive_type ( ASSIGN expList)? SEMI;//
+
+variableList:ID iDlist; //
+
+single_statement: if_statements 
+                | assignment_statements 
+                | foreach_statements 
+                | break_statements 
+                | continue_statements 
+                | return_statements
+                | method_invocations
+                | variable_constant_declaration
+                | block_statements; //
+
+// NOTE: block in block
+// ID in new Expr
+// delemeber access
+// Array lit can be empty
+
+
+
+
+
+
+
+// class declaration
+class_declaration:CLASS  ID (COLON ID)? LP  attributes_methods_declarations RP;//
+
+attributes_methods_declarations: attribute_declaration the_rest_attributes_methods_declarations
+                               | method_declaration the_rest_attributes_methods_declarations
+                               |;//
+
+the_rest_attributes_methods_declarations: attribute_declaration the_rest_attributes_methods_declarations
+                                        | method_declaration the_rest_attributes_methods_declarations
+                                        |;//
+/// the IDlist's length is not equal to the assignment list
+arrayDeclaration: ARRAYTYPE  '[' primitive_type COMMA INTLIT ']'; //
+
+attribute_declaration: (VAR | VAL) attributesList COLON primitive_type ( ASSIGN expList)? SEMI;//
+// assignment: ID ASSIGN exp;
+attributesList: (DOLLARID| ID) iDlist;//
+
+iDlist: COMMA (DOLLARID | ID) iDlist |;//
+
+expList: exp theRestExp;//
+
+theRestExp: COMMA exp theRestExp |;//
+
+primitive_type: BOOLEANTYPE | INTTYPE| FLOATTYPE | STRINGTYPE | arrayDeclaration | ID;//
+
+method_declaration: (DOLLARID | ID) LB list_parameters RB  block_statements
+                  | constructor
+                  | destructor;//
+
+
+list_parameters: parameters_declaration the_rest_parameters_declarations |;//
+
+the_rest_parameters_declarations: SEMI parameters_declaration the_rest_parameters_declarations |;//
+
+parameters_declaration: same_type_parameters COLON primitive_type;//
+
+same_type_parameters: ID the_rest_ID |;//
+
+the_rest_ID: COMMA ID the_rest_ID |;//
+
+constructor: CONSTRUCTOR LB list_parameters RB block_statements;
+
+destructor: DESTRUCTOR LB RB block_statements;
+
+
+
+
+
+
+
+LB: '(';
+
+RB: ')';
+
+LP: '{';
+
+RP: '}';
+
+SEMI: ';';
+
+COMMA: ',';
+
+COLON: ':';
+
+// Key words
+BREAK: 'Break';
+CONTINUE: 'Continue';
+IF: 'If';
+ELSEIF: 'Elseif';
+ELSE: 'Else';
+FOREACH: 'Foreach';
+IN: 'In';
+RETURN: 'Return';
+NULL: 'Null';
+CLASS: 'Class';
+VAL: 'Val';
+VAR: 'Var';
+CONSTRUCTOR: 'Constructor';
+DESTRUCTOR: 'Destructor';
+NEW: 'New';
+BY: 'By';
+SELF: 'Self';
+// Comment skipping
+//UNTERMINATED_COMMENT:  '##' ('#' ~'#' | ~'#')*? EOF;
+
+BlockComment 
+    : '##' .*? '##' -> skip
+    ;
+
+
+// Operators
+PLUS: '+';
+MINUS: '-';
+MULTIPLY: '*';
+DIVIDE: '/';
+MODULO: '%';
+NEGATE: '!';
+AND: '&&';
+OR: '||';
+EQUAL: '==';
+ASSIGN: '=';
+DIFFERENT: '!=';
+GREATER: '>';
+LESSTHANEQUAL: '<=';
+LESSER: '<';
+GREATERTHANEQUAL: '>=';
+STRINGCOMPARE: '==.';
+STRINGCONCAT: '+.';
+STATIC_ACCESS: '::';
+DOT: '.';
+// Primitive type
+
+
+INTTYPE: 'Int';
+FLOATTYPE: 'Float';
+STRINGTYPE: 'String';
+BOOLEANTYPE: 'Boolean';
+ARRAYTYPE: 'Array';
+VOIDTYPE: 'Void';
+
+// Literal
+//float
+fragment DECIMALPART: '.'[0-9]*;
+// fragment EXPONENTPART: [eE][+-]?[1-9]+([0-9])*
+//                      | [Ee][+-]?[0];
+fragment EXPONENTPART: [eE][+-]?[0-9]+;
+//FLOATLIT: ('0'|([1-9]+[0-9_]*))?(DECIMALPART |EXPONENTPART | (DECIMALPART EXPONENTPART)) {self.text = self.text.replace("_","")};
+FLOATLIT: DEC DECIMALPART EXPONENTPART{self.text = self.text.replace("_","")}
+        | DEC EXPONENTPART {self.text = self.text.replace("_","")}
+        | DECIMALPART EXPONENTPART 
+        | DEC DECIMALPART {self.text = self.text.replace("_","")};
+// Int
+// hai gach lien tiep
+fragment OCT: '0'[1-7]([0-7]|'_'[1-7])*
+            | '0'[0];
+fragment HEX: ('0x'|'0X')[1-9A-F]([0-9A-F]|'_'[0-9A-F])*
+            | ('0x'|'0X')[0];
+fragment BIN: ('0b'|'0B')[1]([01]|'_'[01])*
+            | ('0b'|'0B')[0];
+fragment DEC: '0'|[1-9]([0-9]|'_'[0-9])*;
+// ([1-9]+([0-9]|'_'[0-9]))
+INTLIT: (OCT|HEX|BIN|DEC) {self.text = self.text.replace("_", "")};
+//INTLIT: [1-9_]+ {self.text = self.text.replace("_", "")};
+WS: [ \t\r\n]+ -> skip; // skip spaces, tabs, newlines
+// Boolean
+BOOLEANLIT: ('False'| 'True');
+// String
+ILLEGAL_ESCAPE: '"'(~[\\"]| EscapeSequence)*('\\' ~[btnfr'\\] | '\'' ~'"') {raise IllegalEscape(self.text[1:])};
+fragment EscapeSequence: ('\\b'|'\\f'|'\\r'|'\\n'|'\\t'|'\\\''|'\\\\'| '\'"');
+STRINGLIT: '"'(EscapeSequence |~[\\"])*'"' {self.text = self.text[1:-1]};
+UNCLOSE_STRING: '"'(EscapeSequence |~[\\"])* EOF {raise UncloseString(self.text[1:])};
+DOLLARID: '$'[a-zA-Z_]+[a-zA-Z0-9_]*;
+ID: [a-zA-Z_]+[a-zA-Z0-9_]*;
+ERROR_CHAR: . {raise ErrorToken(self.text)};
+//{raise UncloseString(self.text[1:])}
--- Sequence Programming/initial/initial/src/main/bkool/codegen/CodeGenerator.py
+++ Sequence Programming/initial/initial/src/main/bkool/codegen/CodeGenerator.py
@@ -13,16 +13,20 @@ from Emitter import Emitter
 from Frame import Frame
 from abc import ABC, abstractmethod
 
+
 class CodeGenerator(Utils):
     def __init__(self):
         self.libName = "io"
 
     def init(self):
         return [Symbol("getInt", MType(list(), IntType()), CName(self.libName)),
-                    Symbol("putInt", MType([IntType()], VoidType()), CName(self.libName)),
-                    Symbol("putIntLn", MType([IntType()], VoidType()), CName(self.libName)),
-                    Symbol("putFloat", MType([FloatType()], VoidType()), CName(self.libName))
-                    ]
+                Symbol("putInt", MType([IntType()],
+                       VoidType()), CName(self.libName)),
+                Symbol("putIntLn", MType([IntType()],
+                       VoidType()), CName(self.libName)),
+                Symbol("putFloat", MType(
+                    [FloatType()], VoidType()), CName(self.libName))
+                ]
 
     def gen(self, ast, dir_):
         #ast: AST
@@ -32,13 +36,15 @@ class CodeGenerator(Utils):
         gc = CodeGenVisitor(ast, gl, dir_)
         gc.visit(ast, None)
 
+
 class StringType(Type):
-    
+
     def __str__(self):
         return "StringType"
 
     def accept(self, v, param):
         return None
+
 
 class ArrayPointerType(Type):
     def __init__(self, ctype):
@@ -50,14 +56,19 @@ class ArrayPointerType(Type):
 
     def accept(self, v, param):
         return None
+
+
 class ClassType(Type):
-    def __init__(self,cname):
+    def __init__(self, cname):
         self.cname = cname
+
     def __str__(self):
         return "Class({0})".format(str(self.cname))
+
     def accept(self, v, param):
         return None
-        
+
+
 class SubBody():
     def __init__(self, frame, sym):
         #frame: Frame
@@ -65,6 +76,7 @@ class SubBody():
 
         self.frame = frame
         self.sym = sym
+
 
 class Access():
     def __init__(self, frame, sym, isLeft, isFirst):
@@ -78,8 +90,10 @@ class Access():
         self.isLeft = isLeft
         self.isFirst = isFirst
 
+
 class Val(ABC):
     pass
+
 
 class Index(Val):
     def __init__(self, value):
@@ -87,11 +101,13 @@ class Index(Val):
 
         self.value = value
 
+
 class CName(Val):
     def __init__(self, value):
         #value: String
 
         self.value = value
+
 
 class CodeGenVisitor(BaseVisitor, Utils):
     def __init__(self, astTree, env, dir_):
@@ -109,12 +125,14 @@ class CodeGenVisitor(BaseVisitor, Utils):
         #ast: Program
         #c: Any
 
-        self.emit.printout(self.emit.emitPROLOG(self.className, "java.lang.Object"))
+        self.emit.printout(self.emit.emitPROLOG(
+            self.className, "java.lang.Object"))
         e = SubBody(None, self.env)
         for x in ast.decl:
             e = self.visit(x, e)
         # generate default constructor
-        self.genMETHOD(FuncDecl(Id("<init>"), list(), None, Block(list(), list())), c, Frame("<init>", VoidType))
+        self.genMETHOD(FuncDecl(Id("<init>"), list(), None, Block(
+            list(), list())), c, Frame("<init>", VoidType))
         self.emit.emitEPILOG()
         return c
 
@@ -124,13 +142,15 @@ class CodeGenVisitor(BaseVisitor, Utils):
         #frame: Frame
 
         isInit = consdecl.returnType is None
-        isMain = consdecl.name.name == "main" and len(consdecl.param) == 0 and type(consdecl.returnType) is VoidType
+        isMain = consdecl.name.name == "main" and len(
+            consdecl.param) == 0 and type(consdecl.returnType) is VoidType
         returnType = VoidType() if isInit else consdecl.returnType
         methodName = "<init>" if isInit else consdecl.name.name
         intype = [ArrayPointerType(StringType())] if isMain else list()
         mtype = MType(intype, returnType)
 
-        self.emit.printout(self.emit.emitMETHOD(methodName, mtype, not isInit, frame))
+        self.emit.printout(self.emit.emitMETHOD(
+            methodName, mtype, not isInit, frame))
 
         frame.enterScope(True)
 
@@ -138,16 +158,19 @@ class CodeGenVisitor(BaseVisitor, Utils):
 
         # Generate code for parameter declarations
         if isInit:
-            self.emit.printout(self.emit.emitVAR(frame.getNewIndex(), "this", ClassType(self.className), frame.getStartLabel(), frame.getEndLabel(), frame))
+            self.emit.printout(self.emit.emitVAR(frame.getNewIndex(), "this", ClassType(
+                self.className), frame.getStartLabel(), frame.getEndLabel(), frame))
         if isMain:
-            self.emit.printout(self.emit.emitVAR(frame.getNewIndex(), "args", ArrayPointerType(StringType()), frame.getStartLabel(), frame.getEndLabel(), frame))
+            self.emit.printout(self.emit.emitVAR(frame.getNewIndex(), "args", ArrayPointerType(
+                StringType()), frame.getStartLabel(), frame.getEndLabel(), frame))
 
         body = consdecl.body
         self.emit.printout(self.emit.emitLABEL(frame.getStartLabel(), frame))
 
         # Generate code for statements
         if isInit:
-            self.emit.printout(self.emit.emitREADVAR("this", ClassType(self.className), 0, frame))
+            self.emit.printout(self.emit.emitREADVAR(
+                "this", ClassType(self.className), 0, frame))
             self.emit.printout(self.emit.emitINVOKESPECIAL(frame))
         list(map(lambda x: self.visit(x, SubBody(frame, glenv)), body.stmt))
 
@@ -155,7 +178,7 @@ class CodeGenVisitor(BaseVisitor, Utils):
         if type(returnType) is VoidType:
             self.emit.printout(self.emit.emitRETURN(VoidType(), frame))
         self.emit.printout(self.emit.emitENDMETHOD(frame))
-        frame.exitScope();
+        frame.exitScope()
 
     def visitFuncDecl(self, ast, o):
         #ast: FuncDecl
@@ -175,7 +198,7 @@ class CodeGenVisitor(BaseVisitor, Utils):
         nenv = ctxt.sym
         sym = self.lookup(ast.method.name, nenv, lambda x: x.name)
         cname = sym.value.value
-    
+
         ctype = sym.mtype
 
         in_ = ("", list())
@@ -183,7 +206,8 @@ class CodeGenVisitor(BaseVisitor, Utils):
             str1, typ1 = self.visit(x, Access(frame, nenv, False, True))
             in_ = (in_[0] + str1, in_[1].append(typ1))
         self.emit.printout(in_[0])
-        self.emit.printout(self.emit.emitINVOKESTATIC(cname + "/" + ast.method.name, ctype, frame))
+        self.emit.printout(self.emit.emitINVOKESTATIC(
+            cname + "/" + ast.method.name, ctype, frame))
 
     def visitIntLiteral(self, ast, o):
         #ast: IntLiteral
@@ -192,7 +216,7 @@ class CodeGenVisitor(BaseVisitor, Utils):
         ctxt = o
         frame = ctxt.frame
         return self.emit.emitPUSHICONST(ast.value, frame), IntType()
-    
+
     def visitFloatLiteral(self, ast, o):
         #ast: FloatLiteral
         #o: Any
@@ -200,7 +224,7 @@ class CodeGenVisitor(BaseVisitor, Utils):
         ctxt = o
         frame = ctxt.frame
         return self.emit.emitPUSHFCONST(str(ast.value), frame), FloatType()
-    
+
     def visitBinOP(self, ctx: BinOP, o):
         # Exercise 5 Prgramming Code
         myOp = ctx.op
@@ -219,11 +243,12 @@ class CodeGenVisitor(BaseVisitor, Utils):
             if myOp == '/':
                 print("Type 1", type(type1))
                 print("Type 2", type(type2))
-                return value1 + value2 + self.emit.emitMULOP(myOp,type1, o.frame) + self.emit.emitI2F(o.frame), FloatType()
+                return value1 + value2 + self.emit.emitMULOP(myOp, type1, o.frame) + self.emit.emitI2F(o.frame), FloatType()
             else:
                 return value1 + value2 + self.emit.emitMULOP(myOp, type1, o.frame), type1
         else:
             return value1 + value2 + self.emit.emitREOP(myOp, type1, o.frame), BoolType()
+
     def visitBinExpr(self, ctx, o):
         # Exercise 3 Programing Code
         #ast: BinOP
@@ -236,14 +261,14 @@ class CodeGenVisitor(BaseVisitor, Utils):
         if isinstance(typeE1, FloatType) or isinstance(typeE2, FloatType):
             returnType = FloatType()
         if myOp in ["+", "+."]:
-            return  valueE1 + valueE2 + self.emit.emitADDOP('+', returnType, myFrame), returnType
+            return valueE1 + valueE2 + self.emit.emitADDOP('+', returnType, myFrame), returnType
         elif myOp in ["-", "-."]:
-            return  valueE1 + valueE2 + self.emit.emitADDOP('-', returnType, myFrame), returnType
+            return valueE1 + valueE2 + self.emit.emitADDOP('-', returnType, myFrame), returnType
         elif myOp in ['*', '*.']:
-            return  valueE1 + valueE2 + self.emit.emitMULOP('*', returnType, myFrame), returnType
+            return valueE1 + valueE2 + self.emit.emitMULOP('*', returnType, myFrame), returnType
         else:
-            return  valueE1 + valueE2 + self.emit.emitMULOP('/', returnType, myFrame), returnType
-    
+            return valueE1 + valueE2 + self.emit.emitMULOP('/', returnType, myFrame), returnType
+
     # def visitId(self, ctx: Id, o):
     #     # Exercise 4 Programming Code
     #     symbolList = o.sym
@@ -254,7 +279,7 @@ class CodeGenVisitor(BaseVisitor, Utils):
     #             if type(myIndex) == str:
     #                 return self.emit.emitGETSTATIC(myIndex + '.' + eachSymbol.name, eachSymbol.mtype, myFrame), eachSymbol.mtype
     #             return self.emit.emitREADVAR(eachSymbol.name, eachSymbol.mtype, myIndex, myFrame), eachSymbol.mtype
-    
+
     def visitBinExpr2(self, ctx, o):
         myOp = ctx.op
         value1, type1 = self.visit(ctx.e1, o)
@@ -273,21 +298,23 @@ class CodeGenVisitor(BaseVisitor, Utils):
                 return value1 + value2 + self.emit.emitMULOP(myOp, type1, o.frame), type1
         else:
             return value1 + value2 + self.emit.emitREOP(myOp, type1, o.frame), BoolType()
-    def visitVarDecl(self,ctx,o):
+
+    def visitVarDecl(self, ctx, o):
         # Exercise 6 Programming Code
-        myFrame  = o.frame
+        myFrame = o.frame
         if myFrame is None:
             emittedStr = self.emit.emitATTRIBUTE(ctx.name, ctx.typ, None)
             self.emit.printout(emittedStr)
             return Symbol(ctx.name, ctx.typ, CName("MCClass"))
         else:
-            #myFrame.enterScope(True)
-            emittedStr = self.emit.emitVAR(myFrame.getCurrIndex(), ctx.name, ctx.typ, myFrame.getStartLabel(), myFrame.getEndLabel())
+            # myFrame.enterScope(True)
+            emittedStr = self.emit.emitVAR(myFrame.getCurrIndex(
+            ), ctx.name, ctx.typ, myFrame.getStartLabel(), myFrame.getEndLabel())
             self.emit.printout(emittedStr)
-            #myFrame.exitScope()
+            # myFrame.exitScope()
             result = Symbol(ctx.name, ctx.typ, Index(myFrame.getNewIndex()))
             return result
-    
+
     def visitId(self, ctx: Id, o):
         # Exercise 1
         symbolList = o.sym
@@ -303,13 +330,14 @@ class CodeGenVisitor(BaseVisitor, Utils):
                 if not isLeft:
                     return self.emit.emitREADVAR(eachSymbol.name, eachSymbol.mtype, myIndex, myFrame), eachSymbol.mtype
                 return self.emit.emitWRITEVAR(eachSymbol.name, eachSymbol.mtype, myIndex, myFrame), eachSymbol.mtype
-    
+
     def visitAssign(self, ctx, param):
         # Exercise 2
         rhs = self.visit(ctx.rhs, Access(param.frame, param.sym, False))
-        result = rhs[0] + self.visit(ctx.lhs, Access(param.frame, param.sym, True))[0]
+        result = rhs[0] + self.visit(ctx.lhs,
+                                     Access(param.frame, param.sym, True))[0]
         self.emit.printout(result)
-    
+
     def visitIf(self, ctx, o):
         # Exercise 3
         falseLabel = o.frame.getNewLabel()
@@ -326,8 +354,8 @@ class CodeGenVisitor(BaseVisitor, Utils):
             self.visit(ctx.estmt, o)
         label1 = self.emit.emitLABEL(exitLabel, o.frame)
         self.emit.printout(label1)
-    
-    def visitWhile(self,ctx,o):
+
+    def visitWhile(self, ctx, o):
         # Exercise 4
         o.frame.enterLoop()
         enterLabel = o.frame.getBreakLabel()
@@ -343,21 +371,54 @@ class CodeGenVisitor(BaseVisitor, Utils):
         currCommand = self.emit.emitLABEL(outLabel, o.frame)
         self.emit.printout(currCommand)
         o.frame.exitLoop()
-    
-    def visitFuncDecl(self,ctx,o):
-        currFrame = o.frame
-        currFrame.enterScope(True)
-        startLabel = currFrame.getStartLabel()
-        endLabel = currFrame.getEndLabel()
-        param = ctx.param
-        returnType = ctx.returnType
-        paramType = [x.typ for x in param]
-        mType = MType(paramType, returnType)
-        self.emit.printout(self.emit.emitMETHOD(ctx.name, mType, True, o.frame))
-        for eachParam in param:
-            self.emit.printout(self.emit.emitVAR(currFrame.getNewIndex() -1, eachParam.name, 
-            eachParam.typ, startLabel, endLabel, currFrame))
-            self.visit()
-        self.emit.printout(self.emit.emitLABEL(startLabel,currFrame))
 
-        currFrame.exitScope()
+    def visitFuncDecl(self, ctx, o):
+        paraType = list(map(lambda x: x.typ, ctx.param))
+        funcType = MType(paraType, ctx.returnType)
+        self.emit.printout(self.emit.emitMETHOD(ctx.name, funcType, True))
+        message = SubBody(Frame(ctx.name, ctx.returnType), o.sym.copy())
+        message.frame.enterScope(True)
+        paraSymbolList = list(
+            map(lambda x: x.accept(self, message), ctx.param))
+        message.sym[:0] = paraSymbolList[::-1]
+        varSymbolList = list(
+            map(lambda x: x.accept(self, message), ctx.body[0]))
+        message.sym[:0] = varSymbolList[::-1]
+        self.emit.printout(self.emit.emitLABEL(
+            message.frame.getStartLabel(), message.frame))
+        list(map(lambda b: b.accept(self, message), ctx.body[1]))
+        self.emit.printout(self.emit.emitLABEL(
+            message.frame.getEndLabel(), message.frame))
+        self.emit.printout(self.emit.emitENDMETHOD(message.frame))
+        return Symbol(ctx.name, funcType, CName(self.className))
+
+    def visitBinExpr(self, ctx, o):
+        ast = ctx
+        frame = o.frame
+        (code, _) = ast.e1.accept(self, o)
+        code += self.emit.emitDUP(frame)
+        if ast.op == "&&":
+            false_label = frame.getNewLabel()
+            code += self.emit.emitIFFALSE(false_label, frame)
+            (_code, _) = ast.e2.accept(self, o)
+            code += _code
+            code += self.emit.emitANDOP(frame)
+            end_bin = frame.getNewLabel()
+            code += self.emit.emitGOTO(end_bin, frame)
+            code += self.emit.emitLABEL(false_label, frame)
+            code += self.emit.emitPOP(frame)
+            code += self.emit.emitPUSHICONST("false", frame)
+            code += self.emit.emitLABEL(end_bin, frame)
+        else:
+            true_label = frame.getNewLabel()
+            code += self.emit.emitIFTRUE(true_label, frame)
+            (_code, _) = ast.e2.accept(self, o)
+            code += _code
+            code += self.emit.emitOROP(frame)
+            end_bin = frame.getNewLabel()
+            code += self.emit.emitGOTO(end_bin, frame)
+            code += self.emit.emitLABEL(true_label, frame)
+            code += self.emit.emitPOP(frame)
+            code += self.emit.emitPUSHICONST("true", frame)
+            code += self.emit.emitLABEL(end_bin, frame)
+        return code, BoolType()
